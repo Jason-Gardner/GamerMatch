679b007bf628832dd7c96fbf0478fd7173ad4a91
Jason Gardner <jasonagardner@gmail.com>
2020-03-09 19:18:18 -0400

    Identity accepts username.

--- GamerMatch/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GamerMatch/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,6 +46,10 @@ namespace GamerMatch.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
