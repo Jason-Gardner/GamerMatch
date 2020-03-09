a082b3040e34e2c509c11d343fcca7e86ffa4656
MattBateman <mbatem1882@yahoo.com>
2020-03-09 18:31:16 -0400

    added api call, hid api key, and added basic search

--- GamerMatch/Controllers/HomeController.cs
+++ GamerMatch/Controllers/HomeController.cs
@@ -6,21 +6,60 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using GamerMatch.Models;
+using System.Net.Http;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace GamerMatch.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IConfiguration _config;
+        private JsonDocument jDoc;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            long steamID = 76561198208852060;
+            string gameSearch = "Just Survive";
+
+            ViewBag.ID = await SearchGames(steamID, gameSearch);
+
             return View();
+        }
+
+        public async Task<long> SearchGames(long steamID, string gameSearch)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var apiKey = _config["ApiKey"];
+
+                using (var response =
+                    await httpClient.GetAsync($"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apiKey}&steamid={steamID}&include_appinfo=1"))
+                {
+                    var summary = await response.Content.ReadAsStringAsync();
+                    jDoc = JsonDocument.Parse(summary);
+                    var games = jDoc.RootElement.GetProperty("response").GetProperty("games");
+
+                    for (int i = 0; i < games.GetArrayLength(); i++)
+                    {
+                        var game = games[i].GetProperty("name").GetString();
+
+                        if (game == gameSearch)
+                        {
+                            return steamID;
+                        }
+                    }
+
+                    return 0;
+                }
+            }
         }
 
         public IActionResult Privacy()
